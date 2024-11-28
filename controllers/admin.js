b9fc86f2e527fd0764f10a101710fe9e5e2eab53
Rohit More <rohitmore386@gmail.com>
2024-11-28 21:07:32 +0530

    implemented update and delete user controllers

--- controllers/admin.js
+++ controllers/admin.js
@@ -14,10 +14,30 @@ const getUsers =  async (req, res) => {
 
 
 const updateUser =  async (req, res) => {
+    const updatedData = req.body;
+    
+    try {
+        const user = await User.updateOne(updatedData.id, {
+            $set: updatedData
+        });
+        
+        res.status(200).json({ message: "User successfully updated" });
+    } catch (err) {
+        res.status(400).json({ message: "Error getting user", error: err });
+    }
 
 }
 
 const deleteUser =  async (req, res) => {
+    const { id } = req.params;
+    
+    try {
+        const user = await User.findByIdAndDelete(id);
+        
+        res.status(200).json({ message: "User successfully deleted" });
+    } catch (err) {
+        res.status(400).json({ message: "Error getting user", error: err });
+    }
 
 }
 
--- routes/admin.js
+++ routes/admin.js
@@ -4,13 +4,13 @@ const authorize = require("../middleware/authorize");
 const router = express.Router();
 const { getUsers, updateUser, deleteUser } = require("../controllers/admin");
 
-// /users gives the data of all the users, and for that only Admin and Moderator roles will be authorized
+// This API gives the data of all the users, and for that only Admin and Moderator roles will be authorized
 router.get("/users", protect, authorize([ "Admin", "Moderator" ]), getUsers);
 
-// /users/:id can be used to update the user, and for that only Admin and Moderator roles will be authorized
-router.put("/users/:id", protect, authorize([ "Admin", "Moderator" ]), updateUser);
+// This API can be used to update the user, and for that only Admin and Moderator roles will be authorized
+router.put("/users", protect, authorize([ "Admin", "Moderator" ]), updateUser);
 
-// /delete can be used to delete the user, and for that only Admin and Moderator roles will be authorized
+// This API can be used to delete the user, and for that only Admin and Moderator roles will be authorized
 router.delete("/users/:id", protect, authorize([ "Admin", "Moderator" ]), deleteUser);
 
 module.exports = router;
